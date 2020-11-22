8a971ada708facb7ec1d546861b73690b1c552dd
Alexsandro G Bezerra <alexsandro.bezerra@outlook.com.br>
2020-11-21 21:05:03 -0300

    fix: forgot password button

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -22,11 +22,16 @@ const Route: React.FC<RouteProps> = ({
   return (
     <ReactDOMRoute
       {...rest}
-      render={() => {
+      render={({ location }) => {
         return isPrivate === Boolean(user) ? (
           <Component />
         ) : (
-          <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }} />
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location }
+            }}
+          />
         )
       }}
     />
