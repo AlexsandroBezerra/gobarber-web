b55378cd1313442c1cd8e091cb34b1e7795d612b
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-21 16:07:59 -0300

    test: add input tests

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -52,6 +52,7 @@ const Input: React.FC<InputProps> = ({
       isFilled={isFilled}
       isFocused={isFocused}
       style={containerStyle}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
