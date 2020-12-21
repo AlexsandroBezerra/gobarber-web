7833c7e616c2657b8834cdb9471340601d33e822
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-21 16:03:59 -0300

    test: add signin tests

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useRef } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
@@ -26,6 +26,8 @@ const SignIn: React.FC = () => {
   const { signIn } = useAuth()
   const { addToast } = useToast()
 
+  const history = useHistory()
+
   const handleSubmit = useCallback(
     async (data: SignInDataForm) => {
       try {
@@ -46,6 +48,8 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password
         })
+
+        history.push('/dashboard')
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err)
@@ -62,7 +66,7 @@ const SignIn: React.FC = () => {
         })
       }
     },
-    [signIn, addToast]
+    [signIn, addToast, history]
   )
 
   return (
